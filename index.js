cc481f501b7d5f4ff19e726f6f5d8ce0645c3d53
Jonathan Chan <johnkchan252@gmail.com>
2018-11-01 04:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function prependDriver(name){
 }
 
 function removeLastDriver(){
-  return [...drivers][0,-1];
+  return [...drivers].slice(0,-1);
 }
 
 function removeFirstDriver(){
-  
+  return [...drivers].slice(1);
 }
